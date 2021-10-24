651d24bcf58133a06f6f59202e5e8e70b313f470
Alex <tverdovskyi92@gmail.com>
2021-10-25 00:38:48 +0300

    change url

--- src/actions/user.js
+++ src/actions/user.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 export const feedback = async (name, email, message) => {
   try {
-    const response = await axios.post("http://localhost:3000/api/feedback/", {
-      name,
-      email,
-      message,
-    });
+    const response = await axios.post(
+      "https://feedback-form-tverdovskyi.herokuapp.com/",
+      {
+        name,
+        email,
+        message,
+      }
+    );
     alert(response.data.message);
   } catch (error) {
     alert(error);
